b502dd8c5160482e09f5084bc847a9a44409e796
dong3512 <dlehddls1015@naver.com>
2021-02-26 17:08:16 +0900

    .

--- app/src/main/java/com/eomcs/pms/App.java
+++ app/src/main/java/com/eomcs/pms/App.java
@@ -1,12 +1,47 @@
 package com.eomcs.pms;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedList;
-import com.eomcs.pms.handler.BoardHandler;
-import com.eomcs.pms.handler.MemberHandler;
-import com.eomcs.pms.handler.ProjectHandler;
-import com.eomcs.pms.handler.TaskHandler;
+import java.util.List;
+import com.eomcs.pms.domain.Board;
+import com.eomcs.pms.domain.Member;
+import com.eomcs.pms.domain.Project;
+import com.eomcs.pms.domain.Task;
+import com.eomcs.pms.handler.BoardAddHandler;
+import com.eomcs.pms.handler.BoardDeleteHandler;
+import com.eomcs.pms.handler.BoardDetailHandler;
+import com.eomcs.pms.handler.BoardListHandler;
+import com.eomcs.pms.handler.BoardSearchHandler;
+import com.eomcs.pms.handler.BoardUpdateHandler;
+import com.eomcs.pms.handler.Command;
+import com.eomcs.pms.handler.HelloHandler;
+import com.eomcs.pms.handler.MemberAddHandler;
+import com.eomcs.pms.handler.MemberDeleteHandler;
+import com.eomcs.pms.handler.MemberDetailHandler;
+import com.eomcs.pms.handler.MemberListHandler;
+import com.eomcs.pms.handler.MemberUpdateHandler;
+import com.eomcs.pms.handler.MemberValidatorHandler;
+import com.eomcs.pms.handler.ProjectAddHandler;
+import com.eomcs.pms.handler.ProjectDeleteHandler;
+import com.eomcs.pms.handler.ProjectDetailHandler;
+import com.eomcs.pms.handler.ProjectListHandler;
+import com.eomcs.pms.handler.ProjectUpdateHandler;
+import com.eomcs.pms.handler.TaskAddHandler;
+import com.eomcs.pms.handler.TaskDeleteHandler;
+import com.eomcs.pms.handler.TaskDetailHandler;
+import com.eomcs.pms.handler.TaskListHandler;
+import com.eomcs.pms.handler.TaskUpdateHandler;
 import com.eomcs.util.Prompt;
 
 public class App {
@@ -15,13 +50,58 @@ public class App {
   static ArrayDeque<String> commandStack = new ArrayDeque<>();
   static LinkedList<String> commandQueue = new LinkedList<>();
 
+  // VO 를 저장할 컬렉션 객체
+  static List<Board> boardList;
+  static List<Member> memberList ;
+  static List<Project> projectList;
+  static List<Task> taskList;
 
-  public static void main(String[] args) throws CloneNotSupportedException {
+  // 데이터 파일 정보
+  static File boardFile = new File("boards.data");
+  static File memberFile = new File("members.data");
+  static File projectFile = new File("projects.data");
+  static File taskFile = new File("tasks.data");
 
-    BoardHandler boardHandler = new BoardHandler();
-    MemberHandler memberHandler = new MemberHandler();
-    ProjectHandler projectHandler = new ProjectHandler(memberHandler);
-    TaskHandler taskHandler = new TaskHandler(memberHandler);
+  public static void main(String[] args) {
+
+
+    // 파일에서 데이터를 읽어온다. (데이터 로딩)
+    boardList = loadObjects(boardFile, Board.class);
+    memberList = loadObjects(memberFile, Member.class);
+    projectList = loadObjects(projectFile, Project.class);
+    taskList = loadObjects(taskFile, Task.class);
+
+    // 사용자 명령을 처리하는 객체를 맵에 보관한다.
+    HashMap<String,Command> commandMap = new HashMap<>();
+
+    commandMap.put("/board/add", new BoardAddHandler(boardList));
+    commandMap.put("/board/list", new BoardListHandler(boardList));
+    commandMap.put("/board/detail", new BoardDetailHandler(boardList));
+    commandMap.put("/board/update", new BoardUpdateHandler(boardList));
+    commandMap.put("/board/delete", new BoardDeleteHandler(boardList));
+
+    commandMap.put("/member/add", new MemberAddHandler(memberList));
+    commandMap.put("/member/list", new MemberListHandler(memberList));
+    commandMap.put("/member/detail", new MemberDetailHandler(memberList));
+    commandMap.put("/member/update", new MemberUpdateHandler(memberList));
+    commandMap.put("/member/delete", new MemberDeleteHandler(memberList));
+    MemberValidatorHandler memberValidatorHandler = new MemberValidatorHandler(memberList);
+
+    commandMap.put("/project/add", new ProjectAddHandler(projectList, memberValidatorHandler));
+    commandMap.put("/project/list", new ProjectListHandler(projectList));
+    commandMap.put("/project/detail", new ProjectDetailHandler(projectList));
+    commandMap.put("/project/update", new ProjectUpdateHandler(projectList, memberValidatorHandler));
+    commandMap.put("/project/delete", new ProjectDeleteHandler(projectList));
+
+    commandMap.put("/task/add", new TaskAddHandler(taskList, memberValidatorHandler));
+    commandMap.put("/task/list", new TaskListHandler(taskList));
+    commandMap.put("/task/detail", new TaskDetailHandler(taskList));
+    commandMap.put("/task/update", new TaskUpdateHandler(taskList, memberValidatorHandler));
+    commandMap.put("/task/delete", new TaskDeleteHandler(taskList));
+
+    // 새 기능 추가
+    commandMap.put("/board/search", new BoardSearchHandler(boardList));
+    commandMap.put("/hello", new HelloHandler());
 
     loop:
       while (true) {
@@ -36,66 +116,6 @@ public class App {
 
         try {
           switch (command) {
-            case "/member/add":
-              memberHandler.add();
-              break;
-            case "/member/list":
-              memberHandler.list();
-              break;
-            case "/member/detail":
-              memberHandler.detail();
-              break;  
-            case "/member/update":
-              memberHandler.update();
-              break; 
-            case "/member/delete":
-              memberHandler.delete();
-              break;
-            case "/project/add":
-              projectHandler.add();
-              break;
-            case "/project/list":
-              projectHandler.list();
-              break;
-            case "/project/detail": 
-              projectHandler.detail();
-              break;  
-            case "/project/update":
-              projectHandler.update();
-              break; 
-            case "/project/delete":
-              projectHandler.delete();
-              break;
-            case "/task/add":
-              taskHandler.add();
-              break;
-            case "/task/list":
-              taskHandler.list();
-              break;
-            case "/task/detail": 
-              taskHandler.detail();
-              break;  
-            case "/task/update":
-              taskHandler.update();
-              break; 
-            case "/task/delete":
-              taskHandler.delete();
-              break;
-            case "/board/add":
-              boardHandler.add();
-              break;
-            case "/board/list":
-              boardHandler.list();
-              break;
-            case "/board/detail":
-              boardHandler.detail();
-              break;  
-            case "/board/update":
-              boardHandler.update();
-              break; 
-            case "/board/delete":
-              boardHandler.delete();
-              break;
             case "history":
               printCommandHistory(commandStack.iterator());
               break;
@@ -107,7 +127,15 @@ public class App {
               System.out.println("안녕!");
               break loop;
             default:
-              System.out.println("실행할 수 없는 명령입니다.");
+              Command commandHandler = commandMap.get(command);
+
+              if (commandHandler == null) {
+                System.out.println("실행할 수 없는 명령입니다.");
+              } else {
+                commandHandler.service();
+                // 이제 명령어와 그 명령어를 처리하는 핸들러를 추가할 때 마다
+                // case 문을 추가할 필요가 없다.
+              }
           }
         } catch (Exception e) {
           System.out.println("------------------------------------------");
@@ -117,6 +145,13 @@ public class App {
         }
         System.out.println(); // 이전 명령의 실행을 구분하기 위해 빈 줄 출력
       }
+
+    // 게시글 데이터를 파일로 출력한다.
+
+    saveObjects(boardFile, boardList);
+    saveObjects(memberFile, memberList);
+    saveObjects(projectFile, projectList);
+    saveObjects(taskFile, taskList);
 
     Prompt.close();
   }
@@ -133,4 +168,35 @@ public class App {
       }
     }
   }
+
+  static <T extends Serializable> List<T> loadObjects(File file, Class<T> dataType) {
+    try (ObjectInputStream in = new ObjectInputStream(
+        new BufferedInputStream(
+            new FileInputStream(file)))){
+
+      System.out.printf("파일 %s 로딩!\n",file.getName());
+      return (List<T>) in.readObject();
+
+    }catch (Exception e) {
+      System.out.printf("파일 %s 로딩 중 오류 발생!\n",file.getName());
+      return new ArrayList<T>();
+    }
+  }
+
+  static <T extends Serializable> void saveObjects(File file, List<?> dataList) {
+    try (ObjectOutputStream out = new ObjectOutputStream(
+        new BufferedOutputStream(
+            new FileOutputStream(file)))) {
+
+      out.writeObject(dataList);
+
+      System.out.printf("파일 %s 저장!\n",file.getName());
+
+    } catch (Exception e) {
+      System.out.printf("파일 %s 저장 중 오류 발생!\n",file.getName());
+    }
+  }
+
+
+
 }
